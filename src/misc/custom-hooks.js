3e9ab995be6da911e7ef203014b039a1cc59f012
coderaj2001 <biswajit05101963@gmail.com>
2022-03-04 20:28:57 +0530

    Responsive sidebar using media query

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState,useEffect } from "react";
 
 
 export function useModalState(defaultValue=false){
@@ -10,3 +10,23 @@ export function useModalState(defaultValue=false){
   
   return{isOpen,close,open};
 }
+
+
+
+export const useMediaQuery = query => {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const queryList = window.matchMedia(query);
+    setMatches(queryList.matches);
+
+    const listener = evt => setMatches(evt.matches);
+
+    queryList.addListener(listener);
+    return () => queryList.removeListener(listener);
+  }, [query]);
+
+  return matches;
+};
