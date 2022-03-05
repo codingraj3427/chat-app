c981efbd8251167d90b3db994ca1521e1706a9d4
coderaj2001 <biswajit05101963@gmail.com>
2022-03-05 23:36:27 +0530

    Created the provider block

--- src/Components/dasboard/index.js
+++ src/Components/dasboard/index.js
@@ -3,6 +3,8 @@ import {Drawer,Button, Divider, Alert } from 'rsuite';
 import {useProfile} from '../../context/profile.context';
 import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
+import ProviderBlock from './ProviderBlock';
+
 
 const Dashboard = ({onSignOut}) => {
 
@@ -34,6 +36,7 @@ const onSave= async newData=>
 
     <Drawer.Body>
        <h3>Hey, {profile.name}</h3>
+       <ProviderBlock/>
        <Divider/>  
        <EditableInput
 
