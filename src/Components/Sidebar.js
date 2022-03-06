337f270a585dc294897b9066faf02672fadcb230
coderaj2001 <biswajit05101963@gmail.com>
2022-03-06 22:14:07 +0530

    We have created the chat room

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,18 +1,35 @@
-import React from 'react'
+import React,{useRef,useState,useEffect} from 'react';
+import { Divider } from 'rsuite';
 import CreateRoomBtnModal from './dasboard/CreateRoomBtnModal';
 import DashboardToggle from './dasboard/DashboardToggle';
+import ChatRoomList from './rooms/ChatRoomList';
 
 
 const Sidebar = () => {
-  return (
-    <div className='h-100 pt-2'>
 
-      <div>
+  const topSidebarRef=useRef();
+  const[height,setHeight]=useState(null);
+
+  useEffect(()=>
+  {
+   if(topSidebarRef.current)
+   {
+      setHeight(topSidebarRef.current.scrollHeight)
+   }
+  },[topSidebarRef])
+
+
+
+  return (
+    <div className="h-100 pt-2">
+
+      <div ref={topSidebarRef}>
        <DashboardToggle/>
        <CreateRoomBtnModal/>
+       <Divider>Join The Conversesions</Divider>
       </div>
 
-      bottom
+       <ChatRoomList aboveElHeight={height}/>
       
     </div>
   );
